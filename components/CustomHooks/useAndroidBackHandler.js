e286354694a292e0022c511894e9c0b7b0d6397b
stark <397866153@qq.com>
2021-02-23 17:53:08 +0800

    no message

--- components/CustomHooks/useAndroidBackHandler.js
+++ components/CustomHooks/useAndroidBackHandler.js
@@ -1,13 +1,5 @@
-import React, { useEffect, useRef, useState, useMemo, memo, useCallback, forwardRef,  } from 'react'
-import { Image, View, SafeAreaView, StyleSheet, BackHandler, } from 'react-native'
-import { useDispatch, useSelector } from 'react-redux'
-import PropTypes from 'prop-types'
-import ViewPropTypes from '../ViewPropTypes'
-
-
-useAndroidBackHandler.propTypes = {}
-
-useAndroidBackHandler.defaultProps = {}
+import React, { useEffect, useCallback,   } from 'react'
+import {  BackHandler, } from 'react-native'
 
 /**
  * 监听设备上的后退按钮事件,可以调用你自己的函数来处理后退行为
@@ -22,19 +14,47 @@ useAndroidBackHandler.defaultProps = {}
  * @returns {*}
  * @constructor
  */
+let lastClickTime = (new Date()).valueOf();
 export default function useAndroidBackHandler (props) {
-  const dispatch = useDispatch()
-  const {} = useSelector((state) => state)
-  const { navigation, handleBackPress/*此方法在外部定义时也要 return true,进而 阻止事件冒泡传递*/ } = props
+  const { navigation, handleBackPress/*此方法在外部定义时也要 return true,才能 拦截具体页面的 退出事件*/ } = props
 
-  //If you want to intercept the Android return event on the specific page, override this method
   const _handleBackPress = useCallback(() => {
     console.log('useAndroidBackHandler handleBackPress')
     if (handleBackPress) {
       return handleBackPress()
     }
-    navigation?.goBack()
-    return true //返回true时会阻止事件冒泡传递，因而不会执行默认的后退行为 https://reactnative.cn/docs/backhandler
+
+    /**
+     * 避免安卓用户在首页时按后退按键后直接退出app
+     * Prevent Android users from exiting the app directly after pressing the back button on the homepage
+     */
+    {
+      // console.log('useAndroidBackHandler.js _handleBackPress navigation.dangerouslyGetState()=',navigation.dangerouslyGetState())
+      // const { state } = navigation.dangerouslyGetState().routes[0];
+      // console.log('useAndroidBackHandler.js _handleBackPress state=',state)
+      /**
+       * 若能返回上一页，则不拦截
+       * If you can return to the previous page, do not block
+       */
+      if (navigation.canGoBack()) {
+        return false;
+      } else {
+        let nowTime = (new Date()).valueOf();
+        /**
+         * 2次按下 后退按钮的间隔时间小于1秒才能退出app
+         * The interval between pressing the back button twice is less than 1 second to exit the app
+         */
+        if (nowTime - lastClickTime < 1000) {
+          console.log('useAndroidBackHandler 退出 app');
+          BackHandler.exitApp();
+        } else {
+          console.log('useAndroidBackHandler 再按一次，退出 app');
+          lastClickTime = nowTime;
+        }
+        return true;
+      }
+    }
+    return false //返回false 时不会阻止事件冒泡传递，因而会执行默认的后退行为 https://reactnative.cn/docs/backhandler
   }, [handleBackPress])
 
   /**
@@ -44,7 +64,6 @@ export default function useAndroidBackHandler (props) {
     /*The async keyword cannot be added to the first parameter https://juejin.im/post/6844903985338400782#heading-27 */
     () => {
       console.log('useAndroidBackHandler componentDidMount')
-      // gNavigation = navigation // for NavBackBt.js gNavigation.goBack()
 
       //todo
       BackHandler.addEventListener('hardwareBackPress', _handleBackPress)
@@ -55,14 +74,5 @@ export default function useAndroidBackHandler (props) {
         BackHandler.removeEventListener('hardwareBackPress', _handleBackPress)
       }
     }, [])
-
-  /*
-  componentDidUpdate
-  */
-  useEffect(() => {
-    console.log('useAndroidBackHandler componentDidUpdate,props=', props)
-  })
-
 }
 
-const Styles = StyleSheet.create({})
