c834926ecf94880198b9c520fc400576aba44bc2
stark <397866153@qq.com>
2021-02-26 13:58:18 +0800

    no message

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ import baseModel from './dva/baseModel';
 import models from './dva/models';
 import modelTools from './dva/modelTools';
 import HttpConfig from './api/HttpConfig'; //创建默认的 HTTP配置实例
-import { XHttp } from 'react-native-easy-app';
+import { XHttp, XImage, XText, XView } from 'react-native-easy-app';
 
 const RNProjectTools = {
   baseTimer,
@@ -90,6 +90,9 @@ const RNProjectTools = {
   modelTools,
   HttpConfig,
   XHttp,
+  XView,
+  XImage,
+  XText,
 };
 
 module.exports = RNProjectTools;
